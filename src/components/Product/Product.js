d5b10cff888aed944c41f5605967b85d7c246ec6
Samira Shajahan Borsha <57401425+Samira-Shajahan-Borsha@users.noreply.github.com>
2023-01-30 20:57:29 +0600

    fontAwesome icon added and favicon changed and title changed

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
 const Product = ({ handleAddToCart, product }) => {
-    
+
     // const { handleAddToCart, product } = props;
     const { img, name, price, ratings, seller } = product;
-    
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -16,7 +18,8 @@ const Product = ({ handleAddToCart, product }) => {
                 <p><small>Ratings: {ratings}</small></p>
             </div>
             <button onClick={() => handleAddToCart(product)} className='btn-cart'>
-                <p>Add to Cart</p>
+                <p className='btn-text'>Add to Cart</p>
+                <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>
             </button>
         </div>
     );
